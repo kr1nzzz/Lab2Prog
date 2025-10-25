113fd17e293611953338e8ccd073a11f2aa3f62e
kr1nzzz <timofeyk811@gmail.com>
2025-10-25 12:15:27 +0300

    Initial commit

--- src/Flygon.java
+++ src/Flygon.java
@@ -1,10 +1,9 @@
 import ru.ifmo.se.pokemon.*;
 
-public class Flygon extends Pokemon {
+public class Flygon extends Vibrava {
     public Flygon(String name, int level) {
         super(name, level);
-        setType(Type.GROUND, Type.DRAGON);
         setStats(80, 100, 80, 80, 80, 100);
-        setMove(new FeintAttack(), new RockSlide(), new BugBuzz(), new SonicBoom());
+        setMove(new SonicBoom());
     }
 }
--- src/Hippopotas.java
+++ src/Hippopotas.java
@@ -1,22 +1,11 @@
 import ru.ifmo.se.pokemon.*;
 
 public class Hippopotas extends Pokemon {
-    private String nickname;
 
     public Hippopotas(String name, int level) {
         super(name, level);
-        this.nickname = name;
         setType(Type.GROUND);
         setStats(68, 72, 78, 38, 42, 32);
         setMove(new Bite(), new RockTomb(), new SandAttack());
-    }
-
-    public Pokemon checkEvolution() {
-        if (getLevel() >= 45) {
-            Pokemon evolved = new Hippopowdon(nickname, getLevel());
-            System.out.println(nickname + " эволюционирует в Hippopowdon!");
-            return evolved;
-        }
-        return this;
     }
 }
